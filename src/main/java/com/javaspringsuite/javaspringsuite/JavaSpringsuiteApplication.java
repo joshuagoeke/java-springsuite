27a5d3fbb97db946f6761466e18bc50513fe93af
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-05-19 15:19:57 -0500

    starter code

---
+++ src/main/java/com/javaspringsuite/javaspringsuite/JavaSpringsuiteApplication.java
@@ -0,0 +1,13 @@
+package com.javaspringsuite.javaspringsuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringsuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringsuiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaspringsuite/javaspringsuite/JavaSpringsuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaspringsuite.javaspringsuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringsuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
